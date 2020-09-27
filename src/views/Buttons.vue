b7614bc5b5d19b848f6cdfb665d60d024fb56fee
Krizza Bullecer <krizzabullecer@gmail.com>
2020-09-27 18:31:28 +0800

    init buttons

---
+++ src/views/Buttons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="buttons">
+    <h2>Buttons</h2>
+    <p>Work in progress</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.buttons {
+  margin-top: 30px;
+}
+</style>
