5a93b1d68695db90541200e68bde549923782dc0
Krizza Bullecer <krizzabullecer@gmail.com>
2020-09-28 15:27:38 +0800

    complete buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,262 @@
 <template>
-  <div>
-    <button></button>
+  <div
+    class="button"
+    :class="{
+      clrPrimary: color == 'primary',
+      clrSecondary: color == 'secondary',
+      clrDanger: color == 'danger',
+      sizeSm: size == 'sm',
+      sizeLg: size == 'lg',
+      varOutline: variant == 'outline',
+      varText: variant == 'text',
+      disabledShadow: disableShadow,
+      disabled: disabled
+    }"
+  >
+    <i v-if="startIcon && !endIcon" class="material-icons">
+      {{ startIcon }}
+    </i>
+    <button :disabled="disabled">{{ value }}</button>
+    <i v-if="endIcon && !startIcon" class="material-icons">
+      {{ endIcon }}
+    </i>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "default"
+    },
+    size: {
+      type: String,
+      default: "md"
+    },
+    variant: {
+      type: String,
+      default: "default"
+    },
+    value: {
+      type: String,
+      default: "default"
+    },
+    startIcon: String,
+    endIcon: String,
+    disabled: Boolean,
+    disableShadow: Boolean
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+button {
+  background-color: inherit;
+  border: 0;
+  font-family: "Noto Sans JP", sans-serif;
+  border-radius: 6px;
+  outline: 0;
+  cursor: pointer;
+}
+
+i {
+  font-size: 14px;
+}
+
+.button {
+  display: flex;
+  align-items: center;
+  border-radius: 6px;
+  background: #e0e0e0;
+  color: #3f3f3f;
+  box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+  max-width: max-content;
+  padding: 8px 16px;
+  cursor: pointer;
+
+  &.clrPrimary {
+    background-color: #2962ff;
+
+    button,
+    i {
+      color: #fff;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: #0039cb;
+    }
+  }
+
+  &.clrSecondary {
+    background-color: #455a64;
+
+    button,
+    i {
+      color: #fff;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: #1c313a;
+    }
+  }
+
+  &.clrDanger {
+    background-color: #d32f2f;
+
+    button,
+    i {
+      color: #fff;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: #9a0007;
+    }
+  }
+
+  &:hover,
+  &:focus {
+    background-color: #aeaeae;
+  }
+}
+
+// Variants
+.varOutline {
+  box-shadow: none;
+  background-color: transparent;
+  border: 1px solid #3f3f3f;
+
+  &.clrPrimary {
+    background-color: inherit;
+    border: 1px solid #2962ff;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #2962ff;
+    }
+
+    &:hover,
+    &:focus {
+      border-color: #2962ff;
+      background-color: rgba(41, 98, 255, 0.1);
+    }
+  }
+
+  &.clrSecondary {
+    background-color: inherit;
+    border: 1px solid #455a64;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #455a64;
+    }
+
+    &:hover,
+    &:focus {
+      border-color: #455a64;
+      background-color: rgba(69, 90, 100, 0.1);
+    }
+  }
+
+  &.clrDanger {
+    background-color: inherit;
+    border: 1px solid #d32f2f;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #d32f2f;
+    }
+
+    &:hover,
+    &:focus {
+      border-color: #d32f2f;
+      background-color: rgba(211, 47, 47, 0.1);
+    }
+  }
+}
+
+.varText {
+  background: transparent;
+  box-shadow: none;
+
+  &.clrPrimary {
+    background-color: inherit;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #2962ff;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: rgba(41, 98, 255, 0.1);
+    }
+  }
+
+  &.clrSecondary {
+    background-color: inherit;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #455a64;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: rgba(69, 90, 100, 0.1);
+    }
+  }
+
+  &.clrDanger {
+    background-color: inherit;
+
+    button,
+    i {
+      background-color: transparent;
+      color: #d32f2f;
+    }
+
+    &:hover,
+    &:focus {
+      background-color: rgba(211, 47, 47, 0.1);
+    }
+  }
+}
+
+// Sizes
+.sizeSm {
+  padding: 6px 12px;
+}
+
+.sizeLg {
+  padding: 12px 22px;
+}
+
+.disabled {
+  background-color: #e0e0e0 !important;
+  box-shadow: none !important;
+  cursor: default !important;
+
+  button {
+    color: #9e9e9e !important;
+    cursor: default !important;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: #e0e0e0 !important;
+  }
+}
+
+.disabledShadow {
+  box-shadow: none;
+}
+</style>
--- src/views/Buttons.vue
+++ src/views/Buttons.vue
@@ -1,16 +1,144 @@
 <template>
   <div class="buttons">
     <h2>Buttons</h2>
-    <p>Work in progress</p>
+    <div class="bootstrap-wrapper">
+      <div class="mb-1">
+        <h3>Options</h3>
+        <small>
+          For icons, please refer to this page for possible values:
+          <a href="https://material.io/resources/icons/">
+            https://material.io/resources/icons/
+          </a>
+        </small>
+      </div>
+      <div class="row mb-4">
+        <div class="col-5">
+          Colors:
+          <label>
+            <input type="radio" value="default" v-model="color" /> Default
+          </label>
+          <label>
+            <input type="radio" value="primary" v-model="color" /> Primary
+          </label>
+          <label>
+            <input type="radio" value="secondary" v-model="color" />
+            Secondary
+          </label>
+          <label>
+            <input type="radio" value="danger" v-model="color" /> Danger
+          </label>
+        </div>
+        <div class="col-5">
+          Variants:
+          <label>
+            <input type="radio" value="default" v-model="variant" /> Default
+          </label>
+          <label>
+            <input type="radio" value="outline" v-model="variant" /> Outline
+          </label>
+          <label>
+            <input type="radio" value="text" v-model="variant" /> Text
+          </label>
+        </div>
+        <div class="col-5">
+          Sizes:
+          <label>
+            <input type="radio" value="sm" v-model="size" /> Small
+          </label>
+          <label>
+            <input type="radio" value="md" v-model="size" /> Medium
+          </label>
+          <label>
+            <input type="radio" value="lg" v-model="size" /> Large
+          </label>
+        </div>
+        <div class="col-5">
+          Icons: <input type="checkbox" v-model="icons" />
+          <span v-show="icons">
+            <label>
+              <input type="radio" value="start" v-model="icon" /> Start
+            </label>
+            <input v-show="icon === 'start'" type="text" v-model="startIcon" />
+            <label>
+              <input type="radio" value="end" v-model="icon" /> End
+            </label>
+            <input v-show="icon === 'end'" type="text" v-model="endIcon" />
+          </span>
+        </div>
+        <div class="col-5">
+          Disabled: <input type="checkbox" v-model="disabled" />
+        </div>
+        <div class="col-5">
+          Value:
+          <input type="text" v-model="value" />
+        </div>
+        <div class="col-5">
+          Disable shadow: <input type="checkbox" v-model="noShadow" />
+        </div>
+      </div>
+
+      <div>
+        <p class="text--code">
+          &lt;Button
+          <span v-if="value">value="{{ value }}" </span>
+          <span v-if="color && color !== 'default'">color="{{ color }}" </span>
+          <span v-if="variant">variant="{{ variant }}" </span>
+          <span v-if="size">size="{{ size }}" </span>
+          <span v-if="icons && icon === 'start'">
+            startIcon="{{ startIcon }}"
+          </span>
+          <span v-if="icons && icon === 'end'"> endIcon="{{ endIcon }}" </span>
+          <span v-if="noShadow">disableShadow </span>
+          <span v-if="disabled">disabled </span>
+          /&gt;
+        </p>
+        <Button
+          :color="color"
+          :size="size"
+          :variant="variant"
+          :startIcon="icons && icon !== 'end' ? startIcon : null"
+          :endIcon="icons && icon !== 'start' ? endIcon : null"
+          :value="value"
+          :disabled="disabled"
+          :disableShadow="noShadow"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Button from "@/components/Button.vue";
+
+export default {
+  components: {
+    Button
+  },
+  data: () => ({
+    color: "",
+    size: "",
+    variant: "",
+    icons: false,
+    icon: "start",
+    startIcon: "call",
+    endIcon: "lock",
+    value: "Default",
+    disabled: false,
+    noShadow: false
+  })
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 .buttons {
   margin-top: 30px;
+}
+
+.mb-1 {
+  margin-bottom: 1rem;
+}
+
+.mb-4 {
+  margin-bottom: 4rem;
 }
 </style>
